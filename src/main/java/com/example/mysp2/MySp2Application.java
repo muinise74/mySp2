7c08e734f15406f48b353cbcfa762849f480a592
=문도연 <muinise74@naver.com>
2022-09-16 15:28:22 +0900

    Project 생성

---
+++ src/main/java/com/example/mysp2/MySp2Application.java
@@ -0,0 +1,13 @@
+package com.example.mysp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mysp2/MySp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
