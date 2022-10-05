f07e9a450731015e6d7389ec5aa248c6314f83a5
doyeon <muinise74@naver.com>
2022-10-05 12:58:18 +0900

    google login 1트

--- src/main/java/com/example/mysp2/domain/user/User.java
+++ src/main/java/com/example/mysp2/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.example.mysp2.domain.user;
 
+import com.example.mysp2.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
