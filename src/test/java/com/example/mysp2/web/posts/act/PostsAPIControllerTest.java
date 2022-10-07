3fe6f800084c2bb5ca5b6a485d20f5ec86353be9
doyeon <muinise74@naver.com>
2022-10-07 15:52:50 +0900

    java.lang.NoSuchMethodError: org.springframework.security.authentication.UsernamePasswordAuthenticationToken.authenticated spring-security-test version 문제 -> version을 명시하지 않으니 해결 됨

--- src/test/java/com/example/mysp2/web/posts/act/PostsAPIControllerTest.java
+++ src/test/java/com/example/mysp2/web/posts/act/PostsAPIControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.*;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,7 +25,7 @@ import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
